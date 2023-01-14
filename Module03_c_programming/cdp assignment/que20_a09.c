248fe04960ba4f87cf7b040be4a0856950f253dc
Naim619 <nayemuddinshaikh@gmail.com>
2023-01-14 12:32:13 +0530

    folder

--- Module03_c_programming/cdp assignment/que20_a09.c
+++ Module03_c_programming/cdp assignment/que20_a09.c
@@ -6,11 +6,13 @@ int main()
        // wap to make subtraction of 2 matrix using 2d array.
 
        int arr[2][5] = {{9, 8, 7, 6, 5}, {1, 2, 3, 4, 5}};
+
        int new_arr[5];
 
        for (int j = 0; j < 1; j++)
        {
               for (int i = 0; i < 5; i++)
+
               {
                      new_arr[i] = arr[j][i] * arr[j + 1][i];
               }
